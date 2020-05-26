8d704c6d8c4f65f01b878eb3c94877b63b1c0239
omar villalba <o.kmilovillalba@gmail.com>
2020-05-26 16:57:07 -0500

    agregando archivo python

---
+++ test.py
@@ -0,0 +1,28 @@
+import math
+import numpy as np
+from matplotlib import pyplot as plt
+
+x = np.array(range(1000))*0.1
+y = np.zeros(len(x))
+for i in range(len(x)):
+    y[i] = math.sin(x[i])
+
+x_2 = np.array(range(1000))*0.1
+y_2 = np.zeros(len(x_2))
+for j in range(len(x_2)):
+    y_2[j] = math.cos(x_2[j])
+
+# Creamos el gráfico
+#plt.ion()
+plt.plot(x,y,'b',x_2,y_2,'y')
+
+#Colocamos las etiquetas de los ejes
+plt.xlabel("Coordenada X")
+plt.ylabel("Coordenada Y")
+
+#Colocamos la leyenda
+plt.legend(['Seno','Coseno'])
+
+#Colocamos el título del gráfico
+plt.title("Representacion de funciones")
+plt.show()
