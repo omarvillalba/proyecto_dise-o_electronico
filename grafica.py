161947d049b08090810b299b7f146722d119b087
omarvillalba <32653985+omarvillalba@users.noreply.github.com>
2020-05-27 11:44:16 -0500

    generacion y despliegue de grafica

--- grafica.py
+++ grafica.py
@@ -1,11 +1,15 @@
 #importación de modulos
 import math 
 import numpy as np
+from matplotlib import pyplot as plt
 
 #generaciion de arreglos
 x = np.array(range(20))*0.1
-y = np.array(len(x))
+y = np.zeros(len(x))
 for i in range(len(x)):
     y[i] = math.sin(x[i])
 
 #creacion de graficos
+
+plt.plot(x, y)
+plt.show()
