b46835e37643eb459fc67d486362f0679040f172
oamr villalba <o.kmilovillalba@gmail.com>
2020-05-27 17:53:40 -0500

    Revert "adicionando graficas"
    This reverts commit d1598d447d9df6bfd1660ed3d8edc824ed3fbbc6.

--- grafica.py
+++ grafica.py
@@ -14,46 +14,23 @@ x_2 = np.array(range(200))*0.1
 y_2 = np.zeros(len(x_2))
 for j in range(len(x_2)):
     y_2[j] = math.cos(x_2[j])
-    # Generamos un tercero conjunto de datos para el gráfico
-x_3 = np.array(range(200))*0.1
-y_3 = np.zeros(len(x_3))
-for j in range(len(x_3)):
-    y_3[j] = math.exp(x_3[j])
-    # Generamos un cuarto conjunto de datos para el gráfico
-x_4 = np.array(range(200))*0.1
-y_4 = np.zeros(len(x_4))
-for j in range(len(x_4)):
-    y_4[j] = math.sqrt(x_4[j])
 
 #creacion de graficos
 
-
-plt.subplot(221)
+plt.subplot(211)
 plt.xlabel("eje X")
 plt.ylabel("eje y") 
 plt.legend(['Seno hiperbolico'])
 plt.title("Funcion seno hiperbolico") 
 plt.plot(x,y)
 
-plt.subplot(222)
+plt.subplot(212)
 plt.xlabel("eje X")
 plt.ylabel("eje y") 
 plt.legend(['Coseno'])
 plt.title("Funcion coseno") 
 plt.plot(x_2,y_2)
 
-plt.subplot(223)
-plt.xlabel("eje X")
-plt.ylabel("eje y") 
-plt.legend(['Exponencial'])
-plt.title("Funcion expoencial") 
-plt.plot(x_3,y_3)
-
-plt.subplot(224)
-plt.xlabel("eje X")
-plt.ylabel("eje y") 
-plt.legend(['sqrt'])
-plt.title("Funcion raiz cuadrada") 
-plt.plot(x_4,y_4)
 
 plt.show()
+
