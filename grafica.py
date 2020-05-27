f12cb8ae4a7d85ac7ea4d60758e6c1c69c2cad0f
oamr villalba <o.kmilovillalba@gmail.com>
2020-05-27 15:55:24 -0500

    agregando funcion coseno

--- grafica.py
+++ grafica.py
@@ -9,12 +9,19 @@ y = np.zeros(len(x))
 for i in range(len(x)):
     y[i] = math.sin(x[i])
 
+# Generamos un segundo conjunto de datos para el gráfico
+x_2 = np.array(range(200))*0.1
+y_2 = np.zeros(len(x_2))
+for j in range(len(x_2)):
+    y_2[j] = math.cos(x_2[j])
+
 #creacion de graficos
 plt.title("Funcion seno") 
 
 plt.xlabel("eje X")
-plt.ylabel("eje y")
+plt.ylabel("eje y") 
+plt.legend(['Seno','Coseno'])
 
-plt.plot(x, y)
+plt.plot(x,y,x_2,y_2)
 plt.show()
 
